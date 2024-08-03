9a7593bcb36d6a0844442204c96e8fc305533ea0
xl_z <zhao17843104118@163.com>
2024-08-03 20:19:23 +0800

    add keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<leader>r", "<cmd>s//\\=@a/g<CR>", { desc = "Replace search with @a on line", silent = true })
+map("n", "<leader>R", "<cmd>%s//\\=@a/g<CR>", { desc = "Replace search with @a on file", silent = true })
+
+map("n", "<leader>gn", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "git commit name", silent = true })
+
+map("n", "<C-j>", "6j<CR>", { silent = true })
+map("n", "<C-k>", "6k<CR>", { silent = true })
